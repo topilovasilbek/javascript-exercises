e5c4ae1bc508e65d9501fbabf3f09978537e2c18
topilovasilbek <asilbektback@gmail.com>
2022-11-16 19:59:35 +0500

    Javascript Introduction. Initial concepts

---
+++ 10-11/main.js
@@ -0,0 +1,12 @@
+
+// Data Types in Javascript
+// - Primitive: Number, BigInt, String, Boolean, Null, Undefined, Symbols
+// - Non-primitive(Reference): Objects(Object, Array, Function)
+
+
+
+
+
+// Variables Declaration by var, let and const
+
+console.log(typeof function(){})
