2796c643bd4887c70654e3f5e1aecc03c7246fb6
topilovasilbek <asilbektback@gmail.com>
2022-11-16 19:58:34 +0500

    CLI & Git & GitHub & GitLab

---
+++ 08-11/script.js
@@ -0,0 +1,30 @@
+
+
+// This is second lesson  -  introduction to js. I wrote these code to this file(inside of first homework). Because I was writing this before record tutorial.
+
+// let maxValue = 10;
+
+/* for (let i = 0; i < maxValue; i++) {
+    console.log(i);
+} */
+
+// let i = 0;
+/* do {
+    i++
+    console.log(i);
+} while (i < maxValue) */
+
+/* while (i < maxValue) {
+    console.log(i)
+    i++;
+} */
+
+// Data Types in Javascript:
+// Primitive: Number, String, Boolean, Null, Undefined, Symbols, BigInt;
+// Non-Primitive(Reference): Objects(Object, Array, Function)
+
+
+// Practise
+let number = BigInt(1.1);
+console.log(number)
+console.log(typeof number)
