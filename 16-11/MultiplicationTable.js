e24fe91f790141bed72c00b1acc64169fdb14cca
topilovasilbek <asilbektback@gmail.com>
2022-11-16 22:03:04 +0500

    multiplication table

---
+++ 16-11/MultiplicationTable.js
@@ -0,0 +1,9 @@
+// Karra jadvali
+
+for (let i = 2; i <= 10; i++) {
+  console.log(`${i}lik karra jadvali`);
+  for (let j = 1; j <= 10; j++) {
+    console.log(`${i} * ${j} = ${i * j}`);
+  }
+  console.log();
+}
