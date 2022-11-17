be0d7a67d33cde6f7ef67a768228a5b80214e228
topilovasilbek <asilbektback@gmail.com>
2022-11-17 08:14:14 +0500

    Multiplication table - explanation

--- 16-11/MultiplicationTable.js
+++ 16-11/MultiplicationTable.js
@@ -7,3 +7,8 @@ for (let i = 2; i <= 10; i++) {
   }
   console.log();
 }
+
+// Explanation
+// Bunda nested loop - ya'ni ichma-ich loop bo`ladi, buni zina ichidagi zina deb tushunsak bo`ladi.
+// Har bir 1-loop tsikli uchun ichidagi loop 10 marta aylanib chiqadi.
+// Shu asosda 1-loopdagi i bilan 2-loopdagi j ning ko`paytmasidan karra jadvalini hosil qilyapmiz.
