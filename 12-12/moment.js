8f233f38505235da107cebcffca17611f5007475
topilovasilbek <asilbektback@gmail.com>
2022-12-12 23:06:50 +0500

    moment js library

---
+++ 12-12/moment.js
@@ -0,0 +1,99 @@
+const moment = () => {
+  return {
+    format(param) {
+      let date = new Date();
+      let hours = date.getHours();
+      let minutes = date.getSeconds();
+      let clock12 = hours >= 12 ? "PM" : "AM";
+      let months = [
+        "January",
+        "February",
+        "March",
+        "Aprel",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      let weeks = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+
+      switch (param) {
+        case "LT":
+          return `${hours - 12}:${minutes} ${clock12}`;
+          break;
+        case "LTS":
+          return date.toLocaleTimeString();
+          break;
+        case "L":
+        case "l":
+          return date.toLocaleDateString();
+          break;
+        case "LL":
+          return `${
+            months[date.getMonth()]
+          } ${date.getDate()}, ${date.getFullYear()}`;
+          break;
+        case "ll":
+          return `${months[date.getMonth()].slice(
+            0,
+            3
+          )} ${date.getDate()}, ${date.getFullYear()}`;
+          break;
+        case "LLL":
+          return `${
+            months[date.getMonth()]
+          } ${date.getDate()}, ${date.getFullYear()} ${
+            hours - 12
+          }:${minutes} ${clock12}`;
+          break;
+        case "lll":
+          return `${months[date.getMonth()].slice(
+            0,
+            3
+          )} ${date.getDate()}, ${date.getFullYear()} ${
+            hours - 12
+          }:${minutes} ${clock12}`;
+          break;
+        case "LLLL":
+          return `${weeks[date.getDay()]}, ${
+            months[date.getMonth()]
+          } ${date.getDate()}, ${date.getFullYear()} ${
+            hours - 12
+          }:${minutes} ${clock12}`;
+          break;
+        case "llll":
+          return `${weeks[date.getDay()].slice(0, 3)}, ${months[
+            date.getMonth()
+          ].slice(0, 3)} ${date.getDate()}, ${date.getFullYear()} ${
+            hours - 12
+          }:${minutes} ${clock12}`;
+          break;
+      }
+    },
+  };
+};
+
+// console.log(moment.locale());
+
+// console.log(moment().format("LT"));
+// console.log(moment().format("LTS"));
+// console.log(moment().format("L"));
+// console.log(moment().format("l"));
+// console.log(moment().format("LL"));
+// console.log(moment().format("ll"));
+// console.log(moment().format("LLL"));
+// console.log(moment().format("lll"));
+// console.log(moment().format("LLLL"));
+// console.log(moment().format("llll"));
